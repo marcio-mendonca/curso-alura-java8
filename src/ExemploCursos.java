845c228001630592548a55e6327eac60e1b3e82b
marciomendonca <sb.marcio@hotmail.com>
2019-01-29 23:59:42 -0200

    Mais Streams, Collectors e APIs

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ExemploCursos {
@@ -24,7 +27,7 @@ public class ExemploCursos {
 //		cursos.stream()
 //			.filter(c -> c.getAlunos() > 100)
 //			.forEach(c -> System.out.println(c.getNome()));
-		
+
 //		cursos.stream()
 //		.filter(c -> c.getAlunos() > 100)
 //		.map(c -> c.getAlunos())
@@ -34,11 +37,40 @@ public class ExemploCursos {
 //		.filter(c -> c.getAlunos() > 100)
 //		.map(Curso::getAlunos)
 //		.forEach(System.out::println);
+
+//		int soma = cursos.stream()
+//				   .filter(c -> c.getAlunos() > 100)
+//				   .mapToInt(Curso::getAlunos)
+//				   .sum();
+//		System.out.println(soma);
+
+		Optional<Curso> optional = cursos.stream()
+			.filter(c -> c.getAlunos() > 100)
+			.findAny();
 		
-		int soma = cursos.stream()
-				   .filter(c -> c.getAlunos() > 100)
-				   .mapToInt(Curso::getAlunos)
-				   .sum();
-		System.out.println(soma);
+		//Curso curso = optional.orElse(null);
+
+		optional.ifPresent(c -> System.out.println(c.getNome()));
+		
+		cursos.stream()
+			.filter(c -> c.getAlunos() > 100)
+			.findAny()
+			.ifPresent(c -> System.out.println(c.getNome()));
+		
+		List<Curso> resultados = cursos.stream()
+				.filter(c -> c.getAlunos() > 100)
+				.collect(Collectors.toList());
+		
+		System.out.println("###");
+		resultados.forEach(r -> System.out.println(r.getNome()));
+		
+		cursos.stream() 
+				.filter(c -> c.getAlunos() > 100) 
+				.collect(Collectors.toMap(
+						c -> c.getNome(), 
+						c -> c.getAlunos()))
+				.forEach((nome, alunos) -> System.out.println(nome + " tem "+ alunos + " alunos" ));
+		
+		
 	}
 }
